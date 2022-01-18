42e68af72f427bf6b1a1b456fcea1594b335089b
Minjea Lee <gabe0529@naver.com>
2022-01-18 18:49:57 +0900

    Java Study 2022.01.18

--- jumptojava/ArraySection.java
+++ jumptojava/ArraySection.java
@@ -2,7 +2,20 @@ package jumptojava;
 
 public class ArraySection {
     public static void main(String[] args){
-        
+        String[] args1 = new String[7];
+        // String[] args2 = new String[]; --> error
+        args1[0] = "A";
+        args1[1] = "B";
+        for(int i = 2; i < args1.length; i++){
+            args1[i] = Integer.toString(i); // int --> String
+        }
+
+        for(int i = 0; i < args1.length; i++){ 
+            System.out.println(args1[i]);
+        }
+
+        // System.out.println(args1[7]); --> error
+
+
     }
-    
 }
